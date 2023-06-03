612cbb2fc34fc4bde7d669614bf79b0db2ecaff1
Misha3343343 <129310872+AndrianaOz@users.noreply.github.com>
2023-06-03 18:52:40 +0300

    онп
    баг

--- main.py
+++ main.py
@@ -23,11 +23,12 @@ POISON_IMAGE = pygame.transform.scale(
     pygame.image.load("poison.png"), (SWEET_WIDTH, SWEET_HEIGHT)
 )
 SPEED_IMAGE = pygame.transform.scale(
-    pygame.image.load("speedcandy.png"), (SWEET_WIDTH, SWEET_HEIGHT)
+    pygame.image.load("speed_candy.png"), (SWEET_WIDTH, SWEET_HEIGHT)
 )
 HEART_IMAGE = pygame.transform.scale(
     pygame.image.load("life.png"), (HEART_WIDTH, HEART_HEIGHT)
 )
+
 
 class Sprite:
     def __init__(self, width, height, start_coordinates, speed, image):
@@ -72,6 +73,7 @@ class Sweet(Sprite):
     def fly(self):
         self.change_coordinates(y=self.speed)
 
+
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -95,16 +97,16 @@ class Game:
                     pygame.quit()
                     sys.exit()
 
-            if random.randint(0, 500) == 1:
+            if random.randint(0, 300) == 1:
                 self.sweets.append(
                     Sweet(SWEET_WIDTH, SWEET_HEIGHT, (random.randint(0, WIDTH - SWEET_WIDTH), -50), 3, SWEET_IMAGE))
                 self.sweets.append(
                     Sweet(SWEET_WIDTH, SWEET_HEIGHT, (random.randint(0, WIDTH - SWEET_WIDTH), 0), 3, SWEET2_IMAGE))
             if random.randint(0, 1000) == 1:
-                self.sweets.append(
-                    Sweet(SWEET_WIDTH, SWEET_HEIGHT, (random.randint(0, WIDTH - SWEET_WIDTH), 0), 3, POISON_IMAGE))
-                self.sweets.append(
-                    Sweet(SWEET_WIDTH, SWEET_HEIGHT, (random.randint(0, WIDTH - SWEET_WIDTH), 1), 3, SPEED_IMAGE))
+                self.poison = Sweet(SWEET_WIDTH, SWEET_HEIGHT, (random.randint(0, WIDTH - SWEET_WIDTH), -50), 3, POISON_IMAGE)
+                self.candspeed = Sweet(SWEET_WIDTH, SWEET_HEIGHT, (random.randint(0, WIDTH - SWEET_WIDTH), -50), 3, SPEED_IMAGE)
+
+                self.sweets += [self.poison, self.candspeed]
 
             self.screen.blit(BACKGROUND_IMAGE, (0, 0))
             for i in range(self.hearts):
@@ -113,21 +115,19 @@ class Game:
             self.player.move(pygame.key.get_pressed())
             self.screen.blit(PLAYER_IMAGE, self.player.get_coordinates())
 
+            if self.player.rect.colliderect(self.poison.rect):
+                self.player.speed -= 1
+            if self.player.rect.colliderect(self.candspeed.rect):
+                self.player.speed += 1
+
+
             for sweet in self.sweets:
                 self.screen.blit(sweet.image, sweet.get_coordinates())
-
-                if self.candspeed.rect.colliderect(self.player.rect):
-                    self.speed += 1
-
-                if self.poison.rect.colliderect(self.player.rect):
-                    self.speed -= 1
-
 
                 if sweet.rect.colliderect(self.player.rect):
                     self.sweets.remove(sweet)
                     if self.hearts < 5:
                         self.hearts += 1
-
 
                 sweet.fly()
                 if sweet.get_coordinates()[1] > HEIGHT - sweet.rect.height:
